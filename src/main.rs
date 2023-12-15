75266a7e2a1718aeac3c442813c9f1c9cfe97c0f
Alex Eduardo Chiaranda <aechiara@gmail.com>
2023-12-15 14:57:34 -0300

    first working version

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+
+    // parse and hold arguments passed from command-line
+    let args: Vec<String> = std::env::args().collect();
+
+    // check if receives at least one argument
+    if args.len() < 2 {
+        println!("Please provide a file path");
+        return;
+    }
+
+    // get file path from arguments
+    let file_path = &args[1];
+
+    // check if file exists
+    let file_path = std::path::Path::new(file_path);
+    if !file_path.is_file() || !file_path.exists() {
+        println!("File [{file_path}] not found");
+        return;
+    }
+
+    let file_content = std::fs::read_to_string(file_path).expect("Should be able to read the file");
+    let total_lines: u32 = file_content.lines().count() as u32;
+
+    println!("{file_path} has {total_lines} lines");
+}
